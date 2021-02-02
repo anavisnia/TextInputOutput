e6242336d4353a389e8029d9e8c6fc88a404011a
Ana Višnevskaja <anavisnia@gmail.com>
2021-02-02 11:44:25 +0200

    surusiota ir ikelta i d.txt

--- src/TextInputOutput/Main.java
+++ src/TextInputOutput/Main.java
@@ -8,7 +8,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) throws FileNotFoundException, UnsupportedEncodingException{
+    public static void main(String[] args) {
         // instruction
         /*
         perskaityti duomenis is treju failu po eilute
